86ac8738bd4a18832a0aea2c7fd3353812c45905
Marcin Świerczyna <marcinswierczyna@yahoo.pl>
2017-12-03 09:45:45 +0100

    clenup authentication module

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-from .models import User
+from authentication.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
